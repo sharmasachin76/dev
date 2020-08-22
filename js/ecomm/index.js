aecb2aaaf3f68dd2b55b80d2dbacfdfe6254d3fd
sharmasachin76 <sharmaji2k@yahoo.com>
2020-08-22 17:39:41 +0530

    ecomm files

--- js/ecomm/index.js
+++ js/ecomm/index.js
@@ -2,71 +2,28 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const userRepo = require('./repositories/users');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
+const adminProductRouter = require('./routes/admin/product');
+const productRouter = require('./routes/products');
+const cartsRouter = require ('./routes/carts');
+const mysql = require('mysql');
+
 
 const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
-app.get('/ecomm',(req,res)=> {
-res.send(`
-
-<form method="POST">
-<table align="center">
-<tr><td><h3>Sign Up</h3</td></tr>
-<tr><td></td></tr>
-<tr><td></td></tr>
-<tr><td></td></tr>
-<tr><td></td></tr>
-<tr><td><input type="text" name="email" placeholder="email"></td></tr>
-<tr><td></td></tr>
-<tr><td><input type="password" name="password" placeholder="password"></td></tr>
-<tr><td></td></tr><tr><td></td></tr>
-<tr><td><input type="submit"></td></tr>
-</table>
-</form>
-
-`);
-});
-/*
-const bodyParser =(req,res,next)=>{
-    if(req.method==="POST"){
-        req.on('data',data=>{
-            const parsed = data.toString('utf8').split('&');
-            const formData = {};  
-            for (let pair of parsed){
-                const[key,value]= pair.split('=');
-                formData[key] = value;
-            }
-           // console.log(formData);
-        req.body = formData;
-        next();
-        });
-    }
-    else{
-        next();
-    }
-};
-*/
-//app.post('/ecomm',bodyParser.urlencoded({extended:true}),(req,res)=> {
-app.post('/ecomm',async (req,res)=> {   
-    console.log(req.body);
-    const {email,passwprd} = req.body;
-    
-    const existingUser = await userRepo.getOneBy({email});
-    if(existingUser){
-        return res.send('Account already exists with given email id');
-    }
-    
-    else{
-        userRepo.create(req.body);
-        return res.send("Account created...");
-    }
-
-
-    
-    });
+app.use(cookieSession({
+    keys :['asdfghjklyiyiuyiuyjj34hkkll']
+}));
+app.use(authRouter);
+app.use(productRouter);
+app.use(adminProductRouter);
+app.use(cartsRouter);
 
 app.listen(3000,()=>{
 
-console.log("Server started and running....")
+console.log("Server started and running....");
 });
--- js/ecomm/repositories/users.js
+++ js/ecomm/repositories/users.js
@@ -1,85 +1,14 @@
-const fs = require('fs');
-const crypto = require('crypto');
 
-class UsersRepository{
-    constructor(filename){
-       if(!filename){
-            throw new Error('Creating a repo requires a filename');
+const Repository = require('./repository');
+
+class UsersRepository extends Repository{
+    async comparePassword(password1, password2){
+        if (password1===password2){
+            return true;
         }
-        this.filename = filename;
-        try{
-            fs.accessSync (this.filename);
-        }
-        catch (err){
-            fs.writeFileSync(this.filename,'[]');    
-        }
+        return false;
     }
 
-    async getAll(){
-        const contents = await fs.promises.readFile(this.filename, {
-            encoding: 'utf8'
-        });
-        const data = JSON.parse(contents);
-        return data;
-    }
-
-    async create(attrs){
-        attrs.id = this.randomId();
-        let records = await this.getAll();
-        records.push(attrs);
-        await this.writeAll(records);
-       
-    }
-    async writeAll(records){
-        await fs.promises.writeFile(this.filename,JSON.stringify(records,null,2));
-    } 
-
-    randomId(){
-        return crypto.randomBytes(4).toString('hex');
-    } 
-
-    async getOne(id){
-        const records = await this.getAll();
-        return records.find(record=> record.id===id);
-
-    }
-
-    async delete(id){
-        const records = await this.getAll();
-        const filteredRecord =  records.filter(record=> record.id!==id);
-        await this.writeAll(filteredRecord);
-
-    }
-
-    async update(id,attrs){
-        const records = await this.getAll();
-        const record =  records.find(record=> record.id===id);
-        if(!record){
-            throw new Error (`Record with ${id} not found`);
-        }
-        else{
-            Object.assign(record,attrs);
-            await this.writeAll(records);
-        }
-
-    }
-    async getOneBy(filters){
-        const records = await this.getAll();
-
-        for (let record of records){
-            let found = true;
-            for (let key in filters){
-                if(record[key] !== filters[key]){
-                    found = false;
-                }
-            }
-            if (found){
-                return record;
-            }
-        }
-        
-
-    }
 }
 
 
